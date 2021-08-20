6d2d4fa350d1e96cffd6aa858477a534e2ec375d
kota65535 <kota65535@gmail.com>
2021-08-20 12:14:20 +0000

    python.yaml

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 set -x
 
 git pull origin main
+cp python.yaml ../docker-compose/
 
 cd ../docker-compose
 docker kill $(docker ps -q -a)
-
 docker-compose -f python.yaml up --build --force-recreate
